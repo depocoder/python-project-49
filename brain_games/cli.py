f956269456cb32a8530a6383e0c41cb93a9e7541
ispopov <ispopov@phoenixit.ru>
2023-09-20 22:11:54 +0300

    add we-make-python-styleguide

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,13 @@
+"""Cli module."""
+import logging
+
 import prompt
+
+logger = logging.getLogger(__name__)
 
 
 def welcome_user():
+    """Func for greetings user."""
     name = prompt.string('May I have your name? ')
-    print(f"Hello, {name}!")
+    logger.info(f"Hello, {name}!")
     return name
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+"""Main braing games module."""
+import logging
+
 from brain_games.cli import welcome_user
+
+logger = logging.getLogger(__name__)
 
 
 def main():
-    print("Welcome to the Brain Games")
+    logging.basicConfig(level=logging.INFO)
+    logger.info("Welcome to the Brain Games")
     welcome_user()
 
 
