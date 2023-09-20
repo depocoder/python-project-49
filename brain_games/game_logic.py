419add93f6cbf0269b8f8704a93c456089c8cef4
ispopov <ispopov@phoenixit.ru>
2023-09-20 23:49:18 +0300

    create brain_calculator.py

--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -1,13 +1,54 @@
 """Additional game logic."""
+import random
+
+import prompt
+
+WINS_TO_END = 3
 
 
-def is_answer_correct(random_digit, user_answer):
-    is_even = (random_digit % 2) == 0
-    correct_answer = "yes" if is_even else "no"
-    if correct_answer == user_answer:
+def is_answer_correct(user_answer, correct_answer):
+    if str(correct_answer) == user_answer:
         return True
     print(
         f"'{user_answer}' is wrong answer ;(. "
         f"Correct answer was '{correct_answer}'.",
     )
     return False
+
+
+def make_calculate_question():
+    first_random_digit = random.randint(1, 100)
+    second_random_digit = random.randint(1, 100)
+    operation = random.choice(['+', '-', '*'])
+    if operation == '+':
+        answer = first_random_digit + second_random_digit
+    elif operation == '-':
+        answer = first_random_digit - second_random_digit
+    else:
+        answer = first_random_digit * second_random_digit
+    question = f"{first_random_digit} {operation} {second_random_digit}"
+    return question, answer
+
+
+def make_even_question():
+    random_digit = random.randint(1, 100)
+    is_even = random_digit % 2 == 0
+    answer = 'yes' if is_even else 'no'
+    return random_digit, answer
+
+
+def game_loop(make_question, name):
+    consecutive_wins = 0
+    while consecutive_wins != WINS_TO_END:
+        question, answer = make_question()
+        print(f"Question: {question}")
+        user_answer = prompt.string('Your answer: ').lower()
+        if is_answer_correct(user_answer.lower(), answer):
+            print("Correct")
+            consecutive_wins += 1
+            continue
+
+        print(f"Let's try again, {name}!")
+        consecutive_wins = 0
+
+    print(f"Congratulations, {name}!")
--- brain_games/scripts/brain_calculator.py
+++ brain_games/scripts/brain_calculator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Main braing games module."""
 from brain_games.cli import welcome_user
-from brain_games.game_logic import game_loop, make_even_question
+from brain_games.game_logic import game_loop, make_calculate_question
 
 
 def main():
     name = welcome_user()
-    game_loop(make_even_question, name)
+    print("What is the result of the expression?")
+    game_loop(make_calculate_question, name)
 
 
 if __name__ == '__main__':
