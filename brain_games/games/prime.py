4197da9064369a0875937f8086f6d31597fe52ca
ispopov <ispopov@phoenixit.ru>
2023-09-22 11:33:41 +0300

    Edit prime game logic

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,8 +6,11 @@ MAX_PRIME_DIGIT = 100
 
 def make_prime_question():
     """Create prime digit question."""
-    random_digit = random.randint(1, MAX_PRIME_DIGIT)
-    is_prime = True
+    random_digit = random.randint(0, MAX_PRIME_DIGIT)
+    if random_digit < 2:
+        is_prime = False
+    else:
+        is_prime = True
     for digit in range(2, random_digit):
         if random_digit % digit == 0:
             is_prime = False
