26cf734388f9c25ffe5aef56e037a2091588111c
ispopov <ispopov@phoenixit.ru>
2023-09-22 11:40:52 +0300

    fix linter error

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,10 +7,9 @@ MAX_PRIME_DIGIT = 100
 def make_prime_question():
     """Create prime digit question."""
     random_digit = random.randint(0, MAX_PRIME_DIGIT)
+    is_prime = True
     if random_digit < 2:
         is_prime = False
-    else:
-        is_prime = True
     for digit in range(2, random_digit):
         if random_digit % digit == 0:
             is_prime = False
