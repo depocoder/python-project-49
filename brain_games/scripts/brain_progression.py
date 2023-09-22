25db96003feb4388fdc7a265336efe37134a78cf
ispopov <ispopov@phoenixit.ru>
2023-09-22 11:17:37 +0300

    fix test

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -7,7 +7,6 @@ from brain_games.games.progression import make_progression_question
 
 def main():
     """Cli progression game."""
-    
     name = welcome_user()
     print('What number is missing in the progression?')
     game_loop(make_progression_question, name)
